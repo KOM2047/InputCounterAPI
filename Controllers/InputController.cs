09057946a5d501cc8d06f7ae0598c749edbe2f9b
KOM2047 <kabeloofentsemodimoeng@gmail.com>
2025-06-03 21:21:29 +0200

    Add files via upload

---
+++ Controllers/InputController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using InputCounterAPI.Models;
+using InputCounterAPI.Services;
+
+namespace InputCounterAPI.Controllers
+{
+    [ApiController]
+    [Route("api/input")]
+    public class InputController : ControllerBase
+    {
+        private readonly InputService _inputService;
+
+        public InputController(InputService inputService)
+        {
+            _inputService = inputService;
+        }
+
+        [HttpPost]
+        public IActionResult ProcessUserInputs([FromBody] UserInputModel model)
+        {
+            var parsedInputs = _inputService.ParseInputs(model.Inputs);
+            var result = _inputService.CountOccurrences(parsedInputs);
+
+            return Ok(result);
+        }
+    }
+}
